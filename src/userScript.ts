8fdc945b906df90af8d478bb6814ee545cda208c
fire332 <96039230+fire332@users.noreply.github.com>
2025-08-20 17:35:33 -0700

    fix adblock for server backoff

--- src/userScript.ts
+++ src/userScript.ts
@@ -14,6 +14,7 @@ document.addEventListener(
 
 import './app_api/index';
 import './adblock.js';
+import './hooks/json-stringify';
 import './shorts.js';
 import './sponsorblock.js';
 import './ui.js';
