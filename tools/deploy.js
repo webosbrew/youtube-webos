ad36bf70723f1d2f119aea8ac23738aff2d62cd7
fire332 <96039230+fire332@users.noreply.github.com>
2025-09-26 09:10:13 -0700

    fix deploy script

--- tools/deploy.js
+++ tools/deploy.js
@@ -6,7 +6,7 @@ import pkgJson from '../package.json' with { type: 'json' };
 process.exit(
   spawnSync(
     normalize('./node_modules/.bin/ares-install'),
-    [normalize(`./youtube.leanback.v4_${pkgJson}_all.ipk`)],
+    [normalize(`./youtube.leanback.v4_${pkgJson.version}_all.ipk`)],
     { stdio: 'inherit', shell: true }
   ).status ?? 0
 );
